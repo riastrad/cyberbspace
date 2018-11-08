d645a6296879fa7b3dc23a3ce769507dcd8d9f18
Josh <josherb4@gmail.com>
2018-11-07 20:46:44 -0500

    modify eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
+const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
+  // This is little trick makes all my css inline (i.e. fast)
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  // This is to keep track of all my posts
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getAllSorted().filter(function(item) {
+      return item.inputPath.match(/^\.\/posts\//) !== null;
+    });
+  });
+
+  // Date formatting stuff
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+  });
+
+  eleventyConfig.addFilter("machineDate", dateObj => {
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-MM-dd");
+  });
+
+  // Make sure assets carry through
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('docs')
 };
